ee2812f09b5df92f33a977249a096f313fb0dc5c
misterA <whoismistera@gmail.com>
2023-07-26 23:51:20 -0700

    fix working server through ngrok + audiocraft yayyyyyyy

--- backend/app.py
+++ backend/app.py
@@ -2,26 +2,35 @@ from flask import Flask, request, jsonify
 import base64
 from flask_cors import CORS
 
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+
+# Using small model, better results would be obtained with `medium` or `large`.
+model = MusicGen.get_pretrained('small')
+print('model loaded')
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
-
+model.set_generation_params(
+    use_sampling=True,
+    top_k=250,
+    duration=30
+)
 @app.route('/generate', methods=['GET'])
 def generate():
+    print('generate called')
     start = request.args.get('start', type=float)
     end = request.args.get('end', type=float)
     text = request.args.get('text', type=str)
-
-    # Here you would generate your audio buffer using the provided parameters.
-    # For example:
-    # audio_buffer = generate_audio(start, end, text)
-
-    # For now, let's use a dummy audio_buffer:
-    audio_buffer = b"Dummy audio buffer"
-
-    # audio_base64 = base64.b64encode(audio_buffer).decode('utf-8')
-    with open("audio.txt") as f:
-        audio_base64 = f.read()
-    # audio_base64 = 'data:audio/wav;base64,' + audio_base64
+    output = model.generate_unconditional(num_samples=1, progress=True)
+    # convert from tensor to numpy to bytes 
+    print(f'output shape: {output.shape}')
+    # add wav file header
+    for idx, one_wav in enumerate(output):
+        audio_write(f'test', one_wav.cpu(), model.sample_rate)
+    # load wav file, convert to base64
+    with open(f'test.wav', 'rb') as f:
+        audio_bytes = f.read()
+    audio_base64 = base64.b64encode(audio_bytes).decode('ascii')
     return jsonify({'audio': audio_base64})
 
 # Function to generate audio. This is a placeholder, fill it with your logic.
@@ -30,4 +39,4 @@ def generate_audio(start, end, text):
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
